731e654df3016991358e26afd8cae5bb9bf4d858
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-26 05:09:15 +0300

    Create a new project

---
+++ CustomStepperUIControl/CustomStepperUIControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomStepperUIControl
+//
+//  Created by Арсентий Халимовский on 26.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
