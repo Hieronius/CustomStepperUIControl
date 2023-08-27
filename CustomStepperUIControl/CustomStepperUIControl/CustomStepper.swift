9705836aaa72e697180f4c88ca816f179d08ec5d
Arsenty Halimovsky <ghost12@yandex.ru>
2023-08-27 04:41:08 +0300

    Add action for +/- buttons

--- CustomStepperUIControl/CustomStepperUIControl/CustomStepper.swift
+++ CustomStepperUIControl/CustomStepperUIControl/CustomStepper.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 final class CustomStepper: UIControl {
+    
+    // MARK: - Public Properties
+    
     var currentValue = 1 {
         didSet {
             currentValue = currentValue > 0 ? currentValue : 0
@@ -15,11 +18,13 @@ final class CustomStepper: UIControl {
         }
     }
     
+    // MARK: - Private Properties
+    
     private lazy var decreaseButton: UIButton = {
         let button = UIButton()
         button.setTitleColor(.black, for: .normal)
         button.setTitle("-", for: .normal)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         return button
     }()
     
@@ -27,7 +32,7 @@ final class CustomStepper: UIControl {
         let button = UIButton()
         button.setTitleColor(.black, for: .normal)
         button.setTitle("+", for: .normal)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
         return button
     }()
     
@@ -38,4 +43,18 @@ final class CustomStepper: UIControl {
         label.font = UIFont.monospacedDigitSystemFont(ofSize: 15, weight: UIFont.Weight.regular)
         return label
     }()
+                         
+    // MARK: - IBActions
+    
+    @objc private func buttonAction(_ sender: UIButton) {
+        switch sender {
+        case decreaseButton:
+            currentValue -= 1
+        case increaseButton:
+            currentValue += 1
+        default:
+            break
+        }
+    }
+            
 }
